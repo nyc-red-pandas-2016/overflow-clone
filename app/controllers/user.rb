536c798dee52ee1184c21c4831cf7f8da61b94fc
Peter Duke & Tony Narisi <pair+p-duke.tonynarisi@devbootcamp.com>
2016-08-10 12:46:13 -0400

    Add sign-in feature

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -5,10 +5,26 @@ end
 post '/users' do
   @user = User.new(params[:user])
   if @user.save
-    # helper function to set cookie
+    session[:id] = @user.id
     redirect '/'
   else
     @errors = @user.errors.full_messages
     erb :'users/register'
+  end
+end
+
+get '/users/login' do
+  erb :'users/login'
+end
+
+put '/users' do
+  @user = User.find_by(username: params[:username])
+  if @user && @user.authenticate(params[:password])
+    session[:id] = @user.id
+    redirect '/'
+  else
+    @username = params[:username]
+    @errors = ["Invalid username and password"]
+    erb :'users/login'
   end
 end
