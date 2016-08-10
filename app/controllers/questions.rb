28b9cb81409145002f7e963d7d1a57955db31ea1
Margaret Kamimoto & Nicholas Wang <pair+mkamimoto.nwang9@devbootcamp.com>
2016-08-10 17:01:30 -0400

    added comment and question func

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,9 +1,6 @@
 get '/questions/new' do
-  unless logged_in?
-    redirect '/'
-  else
-    erb :'/questions/new'
-  end
+  redirect '/' unless logged_in?
+  erb :'/questions/new'
 end
 
 get '/questions/:id' do
@@ -13,18 +10,15 @@ end
 
 post '/questions' do
   @question = Question.new(params[:question])
-  unless logged_in?
-    redirect '/'
-  else
-    if @question.save
-      if request.xhr?
-        #ajax
-      else
-        redirect '/'
-      end
+  redirect '/' unless logged_in?
+  if @question.save
+    if request.xhr?
+      #ajax
     else
-      @errors = @question.errors.full_messages
-      erb :'/questions/new'
+      redirect '/'
     end
+  else
+    @errors = @question.errors.full_messages
+    erb :'/questions/new'
   end
 end
