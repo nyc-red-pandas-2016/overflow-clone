d2a659abd270b68454fbde2635547ba71c2b42f5
Tony Narisi <tonynarisi@gmail.com>
2016-08-11 22:29:14 -0400

    Take away ability to vote from guests who are not signed in

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -1,8 +1,10 @@
 post '/votes/new' do
-  vote = Vote.find_or_initialize_by(user_id: params[:vote][:user_id], votable_id: params[:vote][:votable_id], votable_type: params[:vote][:votable_type])
-  vote.point_value = params[:vote][:point_value]
-  vote.save
-  redirect "/questions/#{params[:question_id]}"
+  if current_user
+    vote = Vote.find_or_initialize_by(user_id: params[:vote][:user_id], votable_id: params[:vote][:votable_id], votable_type: params[:vote][:votable_type])
+    vote.point_value = params[:vote][:point_value]
+    vote.save
+  end
+    redirect "/questions/#{params[:question_id]}"
 end
 
 
